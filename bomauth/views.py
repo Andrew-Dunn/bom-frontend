fd4d55ae17ae16fbeabf67b7709963f57a6d10e7
Dylan McTaggart <s3333915@student.rmit.edu.au>
2014-10-21 08:22:37 +0000

    fixes to logout redirect

--- bomauth/views.py
+++ bomauth/views.py
@@ -18,7 +18,6 @@ def index(request):
    return HttpResponse(html)
 
 def register(request):
-
    if request.method == 'POST':
       form = UserRegisterForm(request.POST)
       userName = request.REQUEST.get('username', None)
@@ -28,11 +27,11 @@ def register(request):
       userLast = request.REQUEST.get('last_name', None)
       if form.is_valid():
          user = User.objects.create_user(username = userName, email = userMail,
-                                         password = userPass)
+               password = userPass)
          user.last_name = userLast;
          user.first_name = userFirst;
          user.save();
-         return HttpResponseRedirect('/')
+         return HttpResponseRedirect('/login/')
       else:
          t = loader.get_template('register.html')
          context = RequestContext(request, { 'form':form })
@@ -53,7 +52,7 @@ def change_password(request):
       html = t.render(context)
       return HttpResponse(html)
    else:
-      return HttpResponseRedirect('/bomauth/login')
+      return HttpResponseRedirect('/login')
 
 def profile(request):
    profile = request.user
